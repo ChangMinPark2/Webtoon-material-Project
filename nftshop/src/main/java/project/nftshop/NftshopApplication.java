5ac20194eddd891d9983b489d2a71d639ff1fdf6
거부길 <sopens@naver.com>
2023-03-17 19:49:58 +0900

    프로젝트 생성

---
+++ nftshop/src/main/java/project/nftshop/NftshopApplication.java
@@ -0,0 +1,13 @@
+package project.nftshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftshopApplication.class, args);
+	}
+
+}
---
+++ nftshop/src/test/java/project/nftshop/NftshopApplicationTests.java
@@ -0,0 +1,13 @@
+package project.nftshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
