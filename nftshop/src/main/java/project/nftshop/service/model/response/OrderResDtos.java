50872f3ccbc7a307dfa452b95210c764ebc43a1e
CHANG MIN PARK <98qkrckdals@naver.com>
2023-05-08 23:23:36 +0900

    Product 수정(디자인)
    디자인

--- nftshop/src/main/java/project/nftshop/service/model/response/OrderResDtos.java
+++ nftshop/src/main/java/project/nftshop/service/model/response/OrderResDtos.java
@@ -1,12 +1,9 @@
 package project.nftshop.service.model.response;
 
 import lombok.*;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Set;
+
 
 public class OrderResDtos {
 
