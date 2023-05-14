a42bedbe524cfbf378580ba63faea24ff09e84ca
거부길 <sopens@naver.com>
2023-05-15 02:23:41 +0900

    updateProduct.html
    상품 수정, 삭제 텍스트 미수정 본

--- nftshop/src/main/resources/templates/main2.js
+++ nftshop/src/main/resources/templates/main2.js
@@ -1,6 +1,6 @@
 const toggleBtn = document.querySelector('.navbar__toogleBtn');
 const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
+const icons = document.querySelector('.navbar__icons');₩
 
 toggleBtn.addEventListener('click', () => {
    menu.classList.toggle('active');
